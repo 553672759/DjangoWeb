c0cabd29dab6f5b3badd1039ffaa1a584c26af28
fantasy <553672759@qq.com>
2019-10-26 10:21:30 +0800

    2019年10月26日

--- DjangoWebProject1/DjangoWebProject1/DBweb/urls.py
+++ DjangoWebProject1/DjangoWebProject1/DBweb/urls.py
@@ -4,8 +4,11 @@ from django.contrib import admin
 from django.conf.urls import url
 
 urlpatterns = [
+    #url(r'',views.login),
     url(r'^admin', admin.site.urls),
-
+    url(r'base', views.base),
+    url('',views.index),
+    
    
 
 ]
--- DjangoWebProject1/DjangoWebProject1/DBweb/views.py
+++ DjangoWebProject1/DjangoWebProject1/DBweb/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
 # Create your views here.
+def login(request):
+    return render(request,"DBweb/login.html")
+
+def base(request):
+    return render(request,"DBweb/base.html")
+
+def index(request):
+    return render(request,"DBweb/index.html")
